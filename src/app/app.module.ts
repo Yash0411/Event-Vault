a17c00406b92532c320dc4ac2a4d484c8ec4323b
Yash0411 <ynm0411@gmail.com>
2020-07-24 15:27:47 +0530

    'EventVault'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,14 +19,14 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { DevelopersComponent } from './developers/developers.component';
 
 const config = {
-  apiKey: "AIzaSyC66xLvoDEQdfFSqWP91BkUrcMgH0FGMcA",
-  authDomain: "potholeapp-adc2b.firebaseapp.com",
-  databaseURL: "https://potholeapp-adc2b.firebaseio.com",
-  projectId: "potholeapp-adc2b",
-  storageBucket: "potholeapp-adc2b.appspot.com",
-  messagingSenderId: "877884572238",
-  appId: "1:877884572238:web:5fa599923cf41f321ef91f",
-  measurementId: "G-Q8XRFPB9MQ"
+  apiKey: "---",
+    authDomain: "---",
+    databaseURL: "---",
+    projectId: "---",
+    storageBucket: "---",
+    messagingSenderId: "---",
+    appId: "---",
+    measurementId: "---"
 };
 
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBcK0mX8IbMF5RxZvg1EfrZi9WAYw1atYk",
-    authDomain: "eventvaultpccoe.firebaseapp.com",
-    databaseURL: "https://eventvaultpccoe.firebaseio.com",
-    projectId: "eventvaultpccoe",
-    storageBucket: "eventvaultpccoe.appspot.com",
-    messagingSenderId: "461052300166",
-    appId: "1:461052300166:web:a2db0ca3973f2c033e3a07",
-    measurementId: "G-YRGRLTXGFK"
+    apiKey: "---",
+    authDomain: "---",
+    databaseURL: "---",
+    projectId: "---",
+    storageBucket: "---",
+    messagingSenderId: "---",
+    appId: "---",
+    measurementId: "---"
 
   }
 };
